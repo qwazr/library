d3fc075a3b5ca164d7c23e967dbebd6c51ac647b
Emmanuel Keller <ekeller@open-search-server.com>
2016-06-11 20:32:06 +0200

    Rename addColumn to setColumn

--- src/main/java/com/qwazr/connectors/TableRealmConnector.java
+++ src/main/java/com/qwazr/connectors/TableRealmConnector.java
@@ -53,14 +53,14 @@ public class TableRealmConnector extends AbstractLibrary implements IdentityMana
 		Set<String> tables = tableService.list(null, true);
 		if (!tables.contains(table_name)) {
 			tableService.createTable(table_name);
-			tableService.addColumn(table_name, login_column,
+			tableService.setColumn(table_name, login_column,
 					new ColumnDefinition(ColumnDefinition.Type.STRING, ColumnDefinition.Mode.INDEXED));
-			tableService.addColumn(table_name, password_column,
+			tableService.setColumn(table_name, password_column,
 					new ColumnDefinition(ColumnDefinition.Type.STRING, ColumnDefinition.Mode.STORED));
-			tableService.addColumn(table_name, roles_column,
+			tableService.setColumn(table_name, roles_column,
 					new ColumnDefinition(ColumnDefinition.Type.STRING, ColumnDefinition.Mode.STORED));
 		}
-		columns = new HashSet<String>();
+		columns = new HashSet<>();
 		columns.add(password_column);
 		columns.add(roles_column);
 	}
