564cc693d626415f285519b639400e888dd910dd
Emmanuel Keller <ekeller@open-search-server.com>
2017-03-10 14:34:11 +0100

    Remove Fop tool

--- src/main/java/com/qwazr/library/tools/ProcessTool.java
+++ src/main/java/com/qwazr/library/tools/ProcessTool.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-package com.qwazr.tools;
+package com.qwazr.library.tools;
 
 import com.qwazr.library.AbstractLibrary;
 import com.qwazr.utils.ScriptUtils;
